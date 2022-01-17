1d8963b2da24c09880eb0cbc8443877f906861ee
Dobroslav Atanasov <dobroslav.l.atanasov@gmail.com>
2022-01-17 14:35:39 +0200

    Renamed constant.

--- src/Common/ConsoleAppTemplate.Constants/GlobalConstants.cs
+++ src/Common/ConsoleAppTemplate.Constants/GlobalConstants.cs
@@ -2,7 +2,7 @@
 
 public static class GlobalConstants
 {
-    public const string CONSOLE_APPLICATION_CONFIG_FILE = "AppConfig.json";
+    public const string APPLICATION_CONFIG_FILE = "AppConfig.json";
 
     public const string LOGGING = "Logging";
     public const string LOG4NET_CORE = "Log4NetCore";
