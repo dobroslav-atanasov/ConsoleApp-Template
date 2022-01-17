c1fbf515ec548186d8f2ad45da6b3ceeff6036bb
Dobroslav Atanasov <dobroslav.l.atanasov@gmail.com>
2022-01-17 14:37:26 +0200

    Implemented Program.cs

--- src/Applications/ConsoleAppTemplate.ConsoleApp/Program.cs
+++ src/Applications/ConsoleAppTemplate.ConsoleApp/Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+using ConsoleAppTemplate.Constants;
+
+namespace ConsoleAppTemplate.ConsoleApp;
+
+public class Program
+{
+    public static async Task Main(string[] args)
+    {
+        var configuration = SetupConfiguration();
+        var services = ConfigurationServiceProvider(configuration);
+
+        var engine = services.GetService<Engine>();
+        if (engine != null)
+        {
+            await engine.RunAsync();
+        }
+    }
+
+    private static ServiceProvider ConfigurationServiceProvider(IConfiguration configuration)
+    {
+        var serviceProvider = new Startup(configuration)
+            .ConfigurationServices()
+            .BuildServiceProvider();
+
+        return serviceProvider;            
+    }
+
+    private static IConfiguration SetupConfiguration()
+    {
+        var configuration = new ConfigurationBuilder()
+            .AddJsonFile(GlobalConstants.APPLICATION_CONFIG_FILE, false, true)
+            .Build();
+
+        return configuration;
+    }
+}
