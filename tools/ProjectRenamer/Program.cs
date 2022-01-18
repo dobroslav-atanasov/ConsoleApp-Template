8e471b123ccae6b17ff6646cb9a0461d1248a298
dobroslav-atanasov <dobroslav.l.atanasov@gmail.com>
2022-01-18 11:50:28 +0200

    Finished the program.

--- tools/ProjectRenamer/Program.cs
+++ tools/ProjectRenamer/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace ProjectRenamer;
+﻿using System.Text;
+
+namespace ProjectRenamer;
 
 public class Program
 {
@@ -34,6 +36,14 @@ public class Program
         StartRenameDirectories(directorypath, projectOldName, projectNewName);
         Console.WriteLine("End rename directories.");
 
+        Console.WriteLine("Start rename files...");
+        StartRenameFiles(directorypath, projectOldName, projectNewName);
+        Console.WriteLine("End rename files.");
+
+        Console.WriteLine("Start rename file contents...");
+        StartRenameFileContents(directorypath, projectOldName, projectNewName);
+        Console.WriteLine("End rename file contents.");
+
         Console.WriteLine("Project renamer end!");
     }
 
@@ -42,6 +52,57 @@ public class Program
         var directories = Directory.GetDirectories(directorypath);
         foreach (var directory in directories)
         {
+            var newDirectoryName = directory.Replace(projectOldName, projectNewName);
+            if (newDirectoryName != directory)
+            {
+                Directory.Move(directory, newDirectoryName);
+            }            
+        }
+
+        directories = Directory.GetDirectories(directorypath);
+        foreach (var directory in directories)
+        {
+            StartRenameDirectories(directory, projectOldName, projectNewName);
+
+        }
+    }
+
+    private static void StartRenameFiles(string directorypath, string projectOldName, string projectNewName)
+    {
+        var files = Directory.GetFiles(directorypath);
+        foreach (var file in files)
+        {
+            var newFileName = file.Replace(projectOldName, projectNewName);
+            if (newFileName != file)
+            {
+                Directory.Move(file, newFileName);
+            }
+        }
+
+        var directories = Directory.GetDirectories(directorypath);
+        foreach (var directory in directories)
+        {
+            StartRenameFiles(directory, projectOldName, projectNewName);
+        }
+    }
+
+    private static void StartRenameFileContents(string directoryPath, string projectOldName, string projectNewName)
+    {
+        var files = Directory.GetFiles(directoryPath);
+        foreach (var file in files)
+        {
+            if (!file.EndsWith(".dll"))
+            {
+                var contents = File.ReadAllText(file);
+                contents = contents.Replace(projectOldName, projectNewName);
+                File.WriteAllText(file, contents, Encoding.UTF8);
+            }
+        }
+
+        var directories = Directory.GetDirectories(directoryPath);
+        foreach (var directory in directories)
+        {
+            StartRenameFileContents(directory, projectOldName, projectNewName);
         }
     }
 }
