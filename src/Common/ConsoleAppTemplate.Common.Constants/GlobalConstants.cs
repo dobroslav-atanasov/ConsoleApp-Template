d530281b91c7e0ebd72eb6aec63061fc9aadc6a9
Dobroslav Atanasov <dobroslav.l.atanasov@gmail.com>
2022-01-19 14:51:48 +0200

    Added project.

---
+++ src/Common/ConsoleAppTemplate.Common.Constants/GlobalConstants.cs
@@ -0,0 +1,11 @@
+﻿namespace ConsoleAppTemplate.Common.Constants;
+
+public static class GlobalConstants
+{
+    public const string APPLICATION_CONFIG_FILE = "AppConfig.json";
+
+    public const string LOGGING = "Logging";
+    public const string LOG4NET_CORE = "Log4NetCore";
+
+    public const string DEFAULT_CONNECTION_STRING = "DefaultConnectionString";
+}
