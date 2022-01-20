8ebddc156bd2175d286e76b2257ffd6b5d3a7731
dobroslav-atanasov <dobroslav.l.atanasov@gmail.com>
2022-01-20 13:43:36 +0200

    Added references to projects. Create 2 functions in interface IHttpService.

--- src/Services/ConsoleAppTemplate.Services/HttpService.cs
+++ src/Services/ConsoleAppTemplate.Services/HttpService.cs
@@ -4,4 +4,13 @@ using Interfaces;
 
 public class HttpService : IHttpService
 {
+    public Task<string> GetAsync(string url, string accessToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<string> PostAsync(string url, string json, string accessToken)
+    {
+        throw new NotImplementedException();
+    }
 }
--- src/Services/ConsoleAppTemplate.Services/Interfaces/IHttpService.cs
+++ src/Services/ConsoleAppTemplate.Services/Interfaces/IHttpService.cs
@@ -2,5 +2,7 @@
 
 public interface IHttpService
 {
+    Task<string> GetAsync(string url, string accessToken);
 
+    Task<string> PostAsync(string url, string json, string accessToken);
 }
