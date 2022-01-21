fe80708c07b995183e26a752e79fce9f6b67aeb3
dobroslav-atanasov <dobroslav.l.atanasov@gmail.com>
2022-01-21 09:24:27 +0200

    Added constructor in class Engine.

--- src/Applications/ConsoleAppTemplate.ConsoleApp/Engine.cs
+++ src/Applications/ConsoleAppTemplate.ConsoleApp/Engine.cs
@@ -1,7 +1,16 @@
-﻿namespace ConsoleAppTemplate.ConsoleApp;
+﻿using ConsoleAppTemplate.Services.Interfaces;
+
+namespace ConsoleAppTemplate.ConsoleApp;
 
 internal class Engine
 {
+    private readonly IHttpService httpService;
+
+    public Engine(IHttpService httpService)
+    {
+        this.httpService = httpService;
+    }
+
     internal async Task RunAsync()
     {
 
