32d06a7b3aa69b75385b74157785ad0ac2743d47
Dobroslav Atanasov <dobroslav.l.atanasov@gmail.com>
2022-01-24 21:45:49 +0200

    Created static class AutoMapperConfiguration. At this file will be create all configuration for automapper.

---
+++ src/Services/ConsoleAppTemplate.Services.Mapper/AutoMapperConfiguration.cs
@@ -0,0 +1,15 @@
+﻿namespace ConsoleAppTemplate.Services.Mapper;
+
+using System.Reflection;
+
+public static class AutoMapperConfiguration
+{
+    public static void ConfigureMappings(params Assembly[] assemblies)
+    {
+        var types = assemblies
+            .SelectMany(a => a.GetExportedTypes())
+            .ToList();
+
+        ;
+    }
+}
