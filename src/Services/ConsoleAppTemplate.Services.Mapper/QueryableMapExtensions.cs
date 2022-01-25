85c6481ec862377ad87d92b9c26bf13210f42026
Dobroslav Atanasov <dobroslav.l.atanasov@gmail.com>
2022-01-25 13:25:18 +0200

    Created extension functions.

---
+++ src/Services/ConsoleAppTemplate.Services.Mapper/QueryableMapExtensions.cs
@@ -0,0 +1,28 @@
+﻿namespace ConsoleAppTemplate.Services.Mapper;
+
+using System.Linq.Expressions;
+
+using AutoMapper.QueryableExtensions;
+
+public static class QueryableMapExtensions
+{
+    public static IQueryable<TDestination> To<TDestination>(this IQueryable source, params Expression<Func<TDestination, object>>[] membersToExpand)
+    {
+        if (source != null)
+        {
+            throw new ArgumentNullException(nameof(source));
+        }
+
+        return source.ProjectTo(AutoMapperConfiguration.MapperInstance.ConfigurationProvider, null, membersToExpand);
+    }
+
+    public static IQueryable<TDestination> To<TDestination>(this IQueryable source, object parameters)
+    {
+        if (source == null)
+        {
+            throw new ArgumentNullException(nameof(source));
+        }
+
+        return source.ProjectTo<TDestination>(AutoMapperConfiguration.MapperInstance.ConfigurationProvider, parameters);
+    }
+}
